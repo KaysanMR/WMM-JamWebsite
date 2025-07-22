8b6029092b4b2b37befeb522983e9c42fa14b96a
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2025-07-23 01:09:42 +0800

    Add footer component and associated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+//add header
 document.addEventListener("DOMContentLoaded", async () => {
   try {
     const response = await fetch("../components/header.html");
@@ -15,5 +17,25 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   } catch (err) {
     console.error("Failed to load header:", err);
+  }
+});
+//add footer
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("../components/footer.html");
+    if (!response.ok) throw new Error("Footer file not found");
+    const footerHTML = await response.text();
+
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = footerHTML;
+    const footer = tempDiv.querySelector("footer");
+
+    if (footer) {
+      document.body.appendChild(footer);
+    } else {
+      console.warn("No <footer> tag found in footer.html");
+    }
+  } catch (err) {
+    console.error("Failed to load footer:", err);
   }
 });
